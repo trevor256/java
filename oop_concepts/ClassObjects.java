0165e8f2249eeb7cbacc9c8d76e81cfd03073da3
trevor256 <256trevor@gmail.com>
2023-09-09 18:31:03 -0400

    add

---
+++ oop_concepts/ClassObjects.java
@@ -0,0 +1,12 @@
+public class Main {
+    int x = 5;
+
+    public static void main (String[] args){
+    Main object1 = new Main();
+    Main object2 = new Main();
+
+    System.out.println(object1.x);
+    System.out.println(object2.x);
+    }
+
+}
